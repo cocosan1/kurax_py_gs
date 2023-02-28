a16654e1bdade5c3ee0eb21aff6eb415477b6d52
hskw1 <isishskw@gmail.com>
2023-02-28 19:33:59 +0900

    11th commit

--- main.py
+++ main.py
@@ -11,132 +11,132 @@ import plotly.graph_objects as go
 st.set_page_config(page_title='kurax_py_gs')
 st.markdown('#### shop仙台 来場者分析')
 
-#APIを叩くURL
-# SP_COPE = [
-#     'https://www.googleapis.com/auth/drive',
-#     'https://spreadsheets.google.com/feeds'  
-# ]
+@st.cache_data
+def get_data():
+    SP_SHEET = 'フォームの回答 1'
 
-SP_SHEET = 'フォームの回答 1'
+    # 秘密鍵jsonファイルから認証情報を取得
+    #第一引数　秘密鍵のpath　第二引数　どこのAPIにアクセスするか
+    #st.secrets[]内は''で囲むこと
+    #scpes 今回実際に使うGoogleサービスの範囲を指定
+    credentials = service_account.Credentials.from_service_account_info(st.secrets['gcp_service_account'], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ])
 
-# 秘密鍵jsonファイルから認証情報を取得
-#第一引数　秘密鍵のpath　第二引数　どこのAPIにアクセスするか
-#st.secrets[]内は''で囲むこと
-#scpes 今回実際に使うGoogleサービスの範囲を指定
-credentials = service_account.Credentials.from_service_account_info(st.secrets['gcp_service_account'], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ])
+    #OAuth2のクレデンシャル（認証情報）を使用してGoogleAPIにログイン
+    gc = gspread.authorize(credentials)
 
-#OAuth2のクレデンシャル（認証情報）を使用してGoogleAPIにログイン
-gc = gspread.authorize(credentials)
+    # IDを指定して、Googleスプレッドシートのワークブックを取得
+    sh = gc.open_by_key(st.secrets['SP_SHHET_KEY'])
 
-# IDを指定して、Googleスプレッドシートのワークブックを取得
-sh = gc.open_by_key(st.secrets['SP_SHHET_KEY'])
+    # シート名を指定して、ワークシートを選択
+    worksheet = sh.worksheet(SP_SHEET)
 
-# シート名を指定して、ワークシートを選択
-worksheet = sh.worksheet(SP_SHEET)
+    data= worksheet.get_all_values()
 
-data= worksheet.get_all_values()
+    # スプレッドシートをDataFrameに取り込む
+    df = pd.DataFrame(data[1:], columns=data[0])
 
-# スプレッドシートをDataFrameに取り込む
-df = pd.DataFrame(data[1:], columns=data[0])
+    #カラム名変更
+    df2 = df.rename(columns={'-': 'timestamp'})
 
-#カラム名変更
-df2 = df.rename(columns={'-': 'timestamp'})
+    #ゼロ埋め
+    miseinen_list = []
+    n20_list = []
+    n30_list = []
+    n40_list = []
+    n50_list = []
+    n60_list = []
+    male_list = []
+    female_list = []
+    for num in df2['年齢層（未成年）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            miseinen_list.append(num)
+        else:
+            miseinen_list.append(0)
 
-#ゼロ埋め
-miseinen_list = []
-n20_list = []
-n30_list = []
-n40_list = []
-n50_list = []
-n60_list = []
-male_list = []
-female_list = []
-for num in df2['年齢層（未成年）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    miseinen_list.append(num)
-  else:
-    miseinen_list.append(0)
+    for num in df2['年齢層（20代）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            n20_list.append(num)
+        else:
+            n20_list.append(0)
 
-for num in df2['年齢層（20代）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    n20_list.append(num)
-  else:
-    n20_list.append(0)
+    for num in df2['年齢層 （30代）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            n30_list.append(num)
+        else:
+            n30_list.append(0)
 
-for num in df2['年齢層 （30代）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    n30_list.append(num)
-  else:
-    n30_list.append(0)
+    for num in df2['年齢層（40代）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            n40_list.append(num)
+        else:
+            n40_list.append(0)
 
-for num in df2['年齢層（40代）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    n40_list.append(num)
-  else:
-    n40_list.append(0)
+    for num in df2['年齢層（50代）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            n50_list.append(num)
+        else:
+            n50_list.append(0)
 
-for num in df2['年齢層（50代）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    n50_list.append(num)
-  else:
-    n50_list.append(0)
+    for num in df2['年齢層（60代）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            n60_list.append(num)
+        else:
+            n60_list.append(0)
 
-for num in df2['年齢層（60代）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    n60_list.append(num)
-  else:
-    n60_list.append(0)
+    for num in df2['性別（男性）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            male_list.append(num)
+        else:
+            male_list.append(0)
 
-for num in df2['性別（男性）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    male_list.append(num)
-  else:
-    male_list.append(0)
+    for num in df2['性別（女性）']:
+        if num in  ['1', '2', '3', '4', '5']:
+            female_list.append(num)
+        else:
+            female_list.append(0)
 
-for num in df2['性別（女性）']:
-  if num in  ['1', '2', '3', '4', '5']:
-    female_list.append(num)
-  else:
-    female_list.append(0)
+    df_temp = pd.DataFrame(list(zip(miseinen_list, n20_list, n30_list, n40_list, n50_list, n60_list, male_list, female_list)),\
+                        columns=df.columns[1:9], index=df.index)
 
-df_temp = pd.DataFrame(list(zip(miseinen_list, n20_list, n30_list, n40_list, n50_list, n60_list, male_list, female_list)),\
-                       columns=df.columns[1:9], index=df.index)
+    #datetime型に変換
+    df2['timestamp'] = pd.to_datetime(df2['timestamp'])
 
-#datetime型に変換
-df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+    #空欄の0埋め処理したdf_tempとtimestampをmerge df3
+    df_date = (df2[['timestamp']])
 
-#空欄の0埋め処理したdf_tempとtimestampをmerge df3
-df_date = (df2[['timestamp']])
+    df3 = df_date.merge(df_temp, left_index=True, right_index=True, how='inner')
 
-df3 = df_date.merge(df_temp, left_index=True, right_index=True, how='inner')
+    #int変換
+    df3['年齢層（未成年）'] = df3['年齢層（未成年）'].astype(int)
+    df3['年齢層（20代）'] = df3['年齢層（20代）'].astype(int)
+    df3['年齢層 （30代）'] = df3['年齢層 （30代）'].astype(int)
+    df3['年齢層（40代）'] = df3['年齢層（40代）'].astype(int)
+    df3['年齢層（50代）'] = df3['年齢層（50代）'].astype(int)
+    df3['年齢層（60代）'] = df3['年齢層（60代）'].astype(int)
+    df3['性別（男性）'] = df3['性別（男性）'].astype(int)
+    df3['性別（女性）'] = df3['性別（女性）'].astype(int)
 
-#int変換
-df3['年齢層（未成年）'] = df3['年齢層（未成年）'].astype(int)
-df3['年齢層（20代）'] = df3['年齢層（20代）'].astype(int)
-df3['年齢層 （30代）'] = df3['年齢層 （30代）'].astype(int)
-df3['年齢層（40代）'] = df3['年齢層（40代）'].astype(int)
-df3['年齢層（50代）'] = df3['年齢層（50代）'].astype(int)
-df3['年齢層（60代）'] = df3['年齢層（60代）'].astype(int)
-df3['性別（男性）'] = df3['性別（男性）'].astype(int)
-df3['性別（女性）'] = df3['性別（女性）'].astype(int)
+    #時刻データを削除した列の新設 str化
+    df3['timestamp2'] = df3['timestamp'].map(lambda x: x.strftime('%Y-%m-%d'))
+    #datetime化
+    df3['timestamp2'] = pd.to_datetime(df3['timestamp2'])
 
-#時刻データを削除した列の新設 str化
-df3['timestamp2'] = df3['timestamp'].map(lambda x: x.strftime('%Y-%m-%d'))
-#datetime化
-df3['timestamp2'] = pd.to_datetime(df3['timestamp2'])
+    df3['timestamp3'] = df3['timestamp'].map(lambda x: x.strftime('%Y-%m'))
+    #datetime化
+    df3['timestamp3'] = pd.to_datetime(df3['timestamp3'])
 
-df3['timestamp3'] = df3['timestamp'].map(lambda x: x.strftime('%Y-%m'))
-#datetime化
-df3['timestamp3'] = pd.to_datetime(df3['timestamp3'])
+    df3['day_name'] = df3['timestamp'].map(lambda x: x.day_name())
+    df3['hour'] = df3['timestamp'].map(lambda x: x.hour)
+    df3['day_num'] = df3['timestamp'].map(lambda x: x.dayofweek)
 
-df3['day_name'] = df3['timestamp'].map(lambda x: x.day_name())
-df3['hour'] = df3['timestamp'].map(lambda x: x.hour)
-df3['day_num'] = df3['timestamp'].map(lambda x: x.dayofweek)
+    df3['hour'] = df3['hour'].astype(str)
+    df3['day_num'] = df3['day_num'].astype(str)
 
-df3['hour'] = df3['hour'].astype(str)
-df3['day_num'] = df3['day_num'].astype(str)
+    df3['total'] = df3.loc[:, '年齢層（未成年）' :'年齢層（60代）'].sum(axis=1)
 
-df3['total'] = df3.loc[:, '年齢層（未成年）' :'年齢層（60代）'].sum(axis=1)
+    return df3
+
+df3 = get_data()
 
 #組数の算出 日にち
 kumi_dict = {}
